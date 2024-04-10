f138eeee2e405ba0e150d8e996dc35a4df0e956b
Stas <maks_maks_96_morozov@mail.ru>
2024-04-10 22:21:52 +0300

    Profile form, controller & notifications are ready

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -13,17 +13,6 @@ class UserLoginForm(AuthenticationForm):
     # username = forms.CharField()
     # password = forms.CharField()
     
-    # username = forms.CharField(
-    #     label='Имя пользователя',
-    #     widget=forms.TextInput(attrs={"autofocus": True,
-    #                                   "class": 'form-control',
-    #                                   "placeholder": 'Введите ваше имя пользователя'}))
-    # password = forms.CharField(
-    #     label='Пароль',
-    #     widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
-    #                                     "class": 'form-control',
-    #                                     "placeholder": 'Введите ваш пароль'})
-    # )
 
 class UserRegistrationForm(UserCreationForm):
 
@@ -44,3 +33,22 @@ class UserRegistrationForm(UserCreationForm):
     email = forms.CharField()
     password1 = forms.CharField()
     password2 = forms.CharField()
+
+
+class ProfileForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = (
+            "image",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+        )
+
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
